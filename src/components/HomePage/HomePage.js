74dce0fb2cb1a2c8adaba947ea13852a16d9d0ba
Grant Bell <grantjbell@gmail.com>
2022-08-11 13:44:08 -0500

    added pick container

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
-import reactflixHeader from "../images/reactflix_header.png"
+import reactflixHeader from "../images/reactflix_header.png";
 
 export const HomePage = () => {
   const [popMovies, setPopMovies] = useState([]);
   const [trendMovies, setTrendMovies] = useState([]);
+  const [moviePick, setMoviePick] = useState({});
   const [actionMovies, setActionMovies] = useState([]);
-  const [searchedMovies, setSearchedMovies] = useState([]);
+  const movieId = Math.floor(Math.random() * 5) + 1;
+
+  useEffect(() => {
+    //location of the fetch is the single movie id in the api
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=e623222adcf057666d3c1dd36f72f3d6`
+    )
+      //retrieve the data from the api in json format
+      .then((res) => res.json())
+      .then((data) => {
+        setMoviePick(data);
+      });
+  }, [movieId]);
+
+  useEffect(() => {
+    //location of the fetch is the customers in the api
+    fetch(`
+        https://api.themoviedb.org/3/movie/popular?api_key=e623222adcf057666d3c1dd36f72f3d6`)
+      //retrieve the data from the api in json format
+      .then((res) => res.json())
+      .then((data) => {
+        //use the second parameter of the useState to make the array of customers from the api in javascript
+        setPopMovies(data.results);
+      });
+  }, []);
 
   useEffect(() => {
     //location of the fetch is the customers in the api
@@ -32,7 +57,8 @@ export const HomePage = () => {
   }, []);
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/discover/movie/?with_genres=28&api_key=e623222adcf057666d3c1dd36f72f3d6`
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie/?with_genres=28&api_key=e623222adcf057666d3c1dd36f72f3d6`
     )
       //retrieve the data from the api in json format
       .then((res) => res.json())
@@ -42,7 +68,8 @@ export const HomePage = () => {
   }, []);
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/discover/movie/?with_genres=28&api_key=e623222adcf057666d3c1dd36f72f3d6`
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie/?with_genres=28&api_key=e623222adcf057666d3c1dd36f72f3d6`
     )
       //retrieve the data from the api in json format
       .then((res) => res.json())
@@ -54,6 +81,30 @@ export const HomePage = () => {
   return (
     <>
       <img src={reactflixHeader} className="site_header"></img>
+      <div className="picks_container">
+        <div className="picks_cont_left">
+          <h1 className="movie_header">
+            {moviePick.original_title
+              ? moviePick.original_title
+              : moviePick.title}
+          </h1>
+          <div className="pick_details">
+            <h2 className="pick_date">{moviePick?.release_date}</h2>
+            <h2 className="pick_runtime">{moviePick?.runtime} min</h2>
+          </div>
+          <div className="pick_overview">{moviePick?.overview}</div>
+        </div>
+        <div className="picks_cont_right">
+          <img
+            className="pick_backdrop"
+            src={
+              moviePick.poster_path
+                ? `https://image.tmdb.org/t/p/original/${moviePick?.poster_path}`
+                : ``
+            }
+          ></img>
+        </div>
+      </div>
       <h2 className="genre_header">Popular Titles</h2>
       <div className="pop_movie_bar">
         <div className="movie_tile">
@@ -73,24 +124,28 @@ export const HomePage = () => {
         </div>
         <h2 className="genre_header">Trending Titles</h2>
         <div className="trend_movie_bar">
-        <div className="movie_tile">
-          {trendMovies.map((movie) => (
-            <Link className="movieBody" key={`${movie.id}`} to={`/${movie.id}`}>
-              <img
-                className="movie_poster"
-                width="200"
-                src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`
-                    : ``
-                }
-              />
-            </Link>
-          ))}
+          <div className="movie_tile">
+            {trendMovies.map((movie) => (
+              <Link
+                className="movieBody"
+                key={`${movie.id}`}
+                to={`/${movie.id}`}
+              >
+                <img
+                  className="movie_poster"
+                  width="200"
+                  src={
+                    movie.poster_path
+                      ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+                      : ``
+                  }
+                />
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-      <h2 className="genre_header">Action Movies</h2>
-      <div className="movie_tile">
+        <h2 className="genre_header">Action Movies</h2>
+        <div className="movie_tile">
           {actionMovies.map((movie) => (
             <Link className="movieBody" key={`${movie.id}`} to={`/${movie.id}`}>
               <img
--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -6,19 +6,8 @@ import "./MoviePage.css"
 export const MoviePage = () => {
   const [movie, setMovie] = useState({});
   const [similarMovies, setSimilarMovies] = useState([]);
-  const { movieId } = useParams();
+  const { movieId } = useParams()
 
-  useEffect(() => {
-    //location of the fetch is the single movie id in the api
-    fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=e623222adcf057666d3c1dd36f72f3d6`
-    )
-      //retrieve the data from the api in json format
-      .then((res) => res.json())
-      .then((data) => {
-        setMovie(data);
-      });
-  }, [movieId]);
 
   useEffect(() => {
     //location of the fetch is the single movie id in the api
